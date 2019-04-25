6b584b9c149d6e923a4e3814fbdf6536b6b6e8c1
elliot_tijou <349251843@qq.com>
2019-04-25 12:17:17 +0800

    fixed start script.

--- go_server_restart.sh
+++ go_server_restart.sh
@@ -3,16 +3,17 @@ GOPATH=/var/lib/jenkins/workspace/jenkins_go_http_server
 BIN_PATH=$GOPATH/bin
 cd $BIN_PATH
 echo "pwd is $PWD"
-COMMAND_GO_SERVER=$BIN_PATH/jenkins_go_http_server
-go_server_pid=`pidof ${COMMAND_GO_SERVER}`
+# COMMAND_GO_SERVER=$BIN_PATH/jenkins_go_http_server
+# go_server_pid=`pidof ${COMMAND_GO_SERVER}`
+go_server_pid=`pidof jenkins_go_http_server` 
 
 if [ $go_server_pid ]; then
 	echo "Pid of Go Server is $go_server_pid"
-	date +"%Y/%m/%d %H:%M:%S-Go Server Restart"
+	date +"%Y/%m/%d %H:%M:%S - Go Server Restart"
 	kill -9 $go_server_pid
 	sudo screen -d -m ./jenkins_go_http_server
-	date +"%Y/%m/%d %H:%M:%S-Restart completed"
+	date +"%Y/%m/%d %H:%M:%S - Restart completed"
 else
 	sudo screen -d -m ./jenkins_go_http_server
-	date +"%Y/%m/%d %H:%M:%S-Start completed"
+	date +"%Y/%m/%d %H:%M:%S - Start completed"
 fi
