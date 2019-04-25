026049b270d5b3174d52a7528fc8811f862f4133
elliot_tijou <349251843@qq.com>
2019-04-25 11:27:07 +0800

    fixed path bug.

--- go_server_restart.sh
+++ go_server_restart.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+GOPATH=$WORKSPACE
 BIN_PATH=$GOPATH/bin
 cd $BIN_PATH
+echo "pwd is $PWD"
 COMMAND_GO_SERVER=$BIN_PATH/jenkins_go_http_server
 go_server_pid=`pidof ${COMMAND_GO_SERVER}`
-echo "Pid of Go Server is $go_server_pid"
 
 if [ $go_server_pid ]; then
+	echo "Pid of Go Server is $go_server_pid"
 	date +"%Y/%m/%d %H:%M:%S-Go Server Restart"
 	kill -9 $go_server_pid
 	sudo screen -d -m ./jenkins_go_http_server
